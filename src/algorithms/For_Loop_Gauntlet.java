16d842f2f063a0eb7e8681275406ac007ecf338a
league <league@wts5.attlocal.net>
2018-09-14 18:25:18 -0700

    For Loop Gauntlet

--- src/algorithms/For_Loop_Gauntlet.java
+++ src/algorithms/For_Loop_Gauntlet.java
@@ -3,52 +3,50 @@ package algorithms;
 import javax.swing.JOptionPane;
 
 public class For_Loop_Gauntlet {
-	 public static void main(String[] args) {
+	public static void main(String[] args) {
 
-	 for(int i = 0; i < 101; i++) { 
-	 System.out.println(i);
-	 }
-	 for(int l = 100; l > 0; l --) {
-		System.out.println(l); 
-	 }
-	 for(int y = 2; y < 101; y++) {
-		 if(y%2==0) {
-			 System.out.println(y);
-		 }
-	 }
-	 for(int t = 1; t < 100; t++) {
-	     if(t%2!=0) {
-	    	 System.out.println(t);
-	     }
-	 }
-	 for(int r = 1; r < 501; r++) {
-		 if(r%2==0) {
-System.out.println(r+" is even.");
-		 }
-		 else if(r%2!=0) {
-System.out.println(r+" is odd.");			 
-		 }
-	 }
-	 for(int p = 0; p < 778; p++) {
-	     if(p%7==0) {
-	    	 System.out.println(p);
-	     }
-	 }
-	
-	
-	for(int x = 2005; x < 2019; x++) {
-		System.out.println("In " + x + " i was " + (x-2005) +" years old" );
+		for (int i = 0; i < 101; i++) {
+			System.out.println(i);
+		}
+		for (int l = 100; l > 0; l--) {
+			System.out.println(l);
+		}
+		for (int y = 2; y < 101; y++) {
+			if (y % 2 == 0) {
+				System.out.println(y);
+			}
+		}
+		for (int t = 1; t < 100; t++) {
+			if (t % 2 != 0) {
+				System.out.println(t);
+			}
+		}
+		for (int r = 1; r < 501; r++) {
+			if (r % 2 == 0) {
+				System.out.println(r + " is even.");
+			} else if (r % 2 != 0) {
+				System.out.println(r + " is odd.");
+			}
+		}
+		for (int p = 0; p < 778; p++) {
+			if (p % 7 == 0) {
+				System.out.println(p);
+			}
+		}
+
+		for (int x = 2005; x < 2019; x++) {
+			System.out.println("In " + x + " i was " + (x - 2005) + " years old");
+		}
+
+		for (int i = 0; i < 3; i++) {
+			for (int s = 0; s < 3; s++) {
+				System.out.println(s + " " + i);
+			}
+		}
+
+		// for(int i = 1; i < 9; i++) {
+
+		// }
+
 	}
-	
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 }
 }
